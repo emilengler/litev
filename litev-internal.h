5be14033098e9025fba440da77dc55cc53d75ba5
Emil Engler <engler+git@unveil2.org>
2022-02-17 08:24:08 +0100

    api: Fix misleading comment
    This commit uses the correct formulation, kernel notification API, for
    the explanation of `struct litev_ev_api`.

--- litev-internal.h
+++ litev-internal.h
@@ -20,7 +20,7 @@
 /* Opaque pointer that holds the data for a kernel notification API. */
 typedef void EV_API_DATA;
 
-/* Structure to define the backend of a kernel event notification API. */
+/* Structure to define the backend of a kernel notification API. */
 struct litev_ev_api {
 	EV_API_DATA	*(*init)(void);
 	void		 (*free)(EV_API_DATA *);
